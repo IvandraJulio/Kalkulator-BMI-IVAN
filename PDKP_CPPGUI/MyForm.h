af2277721c153e74f658a0b6c2a5e5ec2b0a5114
IvandraJulio <ivan.van.bianco@gmail.com>
2024-11-29 12:35:15 +0700

    Update MyForm.h

--- PDKP_CPPGUI/MyForm.h
+++ PDKP_CPPGUI/MyForm.h
@@ -9,24 +9,18 @@ namespace PDKPCPPGUI {
 	using namespace System::Data;
 	using namespace System::Drawing;
 
-	/// <summary>
-	/// Summary for MyForm
-	/// </summary>
+	
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
 	public:
 		MyForm(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: Add the constructor code here
-			//
+			
 		}
 
 	protected:
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
+		
 		~MyForm()
 		{
 			if (components)
@@ -53,16 +47,11 @@ namespace PDKPCPPGUI {
 
 
 	private:
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
+		 
 		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
+		
 		void InitializeComponent(void)
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
